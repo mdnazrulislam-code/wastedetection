41578215cdbe7ae9a4da62bfd1bb8bedc7611fcd
Md Nazrul Islam <arfanislamabir@gmail.com>
2023-12-01 23:46:18 +0600

    Requirements added into the project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Md Nazrul Islam',
+    author_email= 'arfanislamabir@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
